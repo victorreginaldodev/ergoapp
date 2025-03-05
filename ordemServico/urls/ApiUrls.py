27ef78fc42b34bf5e463c90e8f53dfff2c351dfa
ergogit <appergogroup@gmail.com>
2025-03-05 11:25:49 +0000

    Atualizando repo com atualizações do servidor

--- ordemServico/urls/ApiUrls.py
+++ ordemServico/urls/ApiUrls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from ordemServico.views import OrdemServicoViewSet, MiniOsViewSet
+from ordemServico.views.TarefaMiniOSAPIView import TarefaMiniOSAPIView
 
 # Cria o router da DRF
 router = DefaultRouter()
@@ -10,4 +11,5 @@ router.register(r'api/minios', MiniOsViewSet, basename='apiminios')
 # Inclui as URLs geradas pelo router no urlpatterns
 urlpatterns = [
     path('', include(router.urls)),  # Registra todas as rotas criadas pelo router
+    path("api/tarefas-minios/", TarefaMiniOSAPIView.as_view(), name="tarefas-minios"),
 ]
